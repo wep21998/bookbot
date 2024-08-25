a397e835755f2d069a24e0cebd70079a5b9715c9
wep21998 <we.perry.8@hotmail.com>
2024-08-24 21:51:51 -0400

    Added letter count functionality

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 def main():
-    book = open_book()
+    title = f"{sys.argv[1]}.txt"
+    book = open_book(title)
+    characters = character_count(book)
     print(f"There are {word_count(book)} words")
-    print(f"character count is as follows : \n {character_count(book)}")
+    print(f"Begining report on {title}:")
+    for i in range(len(characters)):
+        print(f"The was found {characters[i]} times")
 
 def word_count(book):
     words = book.split()
@@ -14,19 +18,29 @@ def word_count(book):
 
 def character_count(book):
     contents = book.lower()
-    characters = {}
+    characters = []
 
     for character in contents:
-        if characters.get(character) == None:
-            characters[character] = 1
-        else:
-            characters[character] += 1
+        #print(character)
+        #print(characters)
+        #print({"char" : character} in characters)
+        if character.isalpha() == True:
+            if len(characters) == 0: #Determines if character is a letter
+                characters.append({"char" : character, "count" : 1})
+            elif  next((item for item in characters if item["char"] == character), None) == None: #Searches to see if letter is in list
+                characters.append({"char" : character, "count" : 1})
+            else:
+                for i in range(len(characters)):
+                    if characters[i]["char"] == character:
+                        characters[i]["count"] +=1
+    characters.sort(reverse=True, key=sort_on) #Sort by number of letters found
+    print(characters)
     return characters
 
+def sort_on(dict):
+    return dict["count"]
 
-def open_book():
-    title = f"{sys.argv[1]}.txt"
-    #print(title)
+def open_book(title):
     with open(f"books/{title}") as book:
         return book.read()
         
